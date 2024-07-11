dd656ed69cb0b49ebaa1b072815d91b5146ac62e
Kergrit Robkop <kergrit@gmail.com>
2024-07-11 13:57:41 +0700

    add assetPrefix

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
   reactStrictMode: true,
   distDir: 'build',
   output: 'standalone',  
-  // assetPrefix: `${process.env.SKBT_HTTP_HOST}${process.env.SKBT_SUBFOLDER}`,
+  assetPrefix: `${process.env.SKBT_HTTP_HOST}${process.env.SKBT_SUBFOLDER}`,
   // images: {
   //   loader: 'custom',
   //   loaderFile: `./src/image/loader-${process.env.SKBT_BRANCH}.js`,
