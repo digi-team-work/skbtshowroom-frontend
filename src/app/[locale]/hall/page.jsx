c09e4b63c7d5e28ef3c91763e6c3b7de17481f9e
trirong.p <trirong_berm@hotmail.com>
2024-07-18 18:19:43 +0700

    use static hall for present

--- src/app/[locale]/hall/page.jsx
+++ src/app/[locale]/hall/page.jsx
@@ -44,109 +44,109 @@ export default async function HallPage() {
   const productList = await getProducts();
    
 
-  // const items = [
-  //   {
-  //     type : 'product',
-  //     title : "kubota 1", 
-  //     texture : basePath+"/product/trackter_1.png",
-  //     video : [],
-  //     url : linkPath+"/product/1",
-  //   },
-  //   {
-  //     type : 'presenter',
-  //     title : "presenter", 
-  //     texture : basePath+"/product/presenter.png",
-  //     // video : [basePath+"/product/presenter.webm"],
-  //     video : [basePath+"/product/presenter.mp4", basePath+"/product/presenter-alpha.mp4"],
-  //     // video : [],
-  //     url : ""
-  //   },
-  //   {
-  //     type : 'product',
-  //     title : "kubota 2", 
-  //     texture : basePath+"/product/trackter_2.png",
-  //     video : [],
-  //     url : linkPath+"/product/2"
-  //   }, 
-  //   {
-  //     type : 'product',
-  //     title : "kubota 3", 
-  //     texture : basePath+"/product/trackter_3.png",
-  //     video : [],
-  //     url : linkPath+"/product/3"
-  //   }, 
-  //   {
-  //     type : 'product',
-  //     title : "kubota 4", 
-  //     texture : basePath+"/product/trackter_4.png",
-  //     video : [],
-  //     url : linkPath+"/product/4"
-  //   }, 
-  //   {
-  //     type : 'product',
-  //     title : "kubota 5", 
-  //     texture : basePath+"/product/trackter_5.png",
-  //     video : [],
-  //     url : linkPath+"/product/5"
-  //   },
-  //   {
-  //     type : 'product',
-  //     title : "kubota 6", 
-  //     texture : basePath+"/product/trackter_6.png",
-  //     video : [],
-  //     url : linkPath+"/product/6"
-  //   },
-  //   {
-  //     type : 'product',
-  //     title : "kubota 7", 
-  //     texture : basePath+"/product/trackter_7.png",
-  //     video : [],
-  //     url : linkPath+"/product/7"
-  //   }
-  // ];
-  // const picture = [basePath+"/picture-01.png", basePath+"/picture-02.png"];
-  // const presenter = items.find((item, itemKey) => item.type == 'presenter');
-
-  let items = [];
-  productList.products.map((item, itemKey) => {
-    if(item.type == "product"){
-      if(item.image_showroom){
-        items.push({
-          id:item.id,
-          type : item.type,
-          title:"",
-          texture:item.image_showroom,
-          video:[],
-          url:`${linkPath}/product/${item.id}`
-        });
-      }
-    }else if(item.type == "person") {
-      items.push({
-        id:item.id,
-        type : item.type,
-        title:"",
-        texture:item.person.person_image,
-        video:[
-          (item.person.person_video_rgb ? (item.person.person_video_rgb):('')), 
-          (item.person.person_video_alpha ? (item.person.person_video_alpha):(''))
-        ],
-        url:`${linkPath}/product/${item.id}`
-      });
+  const items = [
+    {
+      type : 'product',
+      title : "kubota 1", 
+      texture : basePath+"/product/trackter_1.png",
+      video : [],
+      url : linkPath+"/product/1",
+    },
+    {
+      type : 'presenter',
+      title : "presenter", 
+      texture : basePath+"/product/presenter.png",
+      // video : [basePath+"/product/presenter.webm"],
+      video : [basePath+"/product/presenter.mp4", basePath+"/product/presenter-alpha.mp4"],
+      // video : [],
+      url : ""
+    },
+    {
+      type : 'product',
+      title : "kubota 2", 
+      texture : basePath+"/product/trackter_2.png",
+      video : [],
+      url : linkPath+"/product/2"
+    }, 
+    {
+      type : 'product',
+      title : "kubota 3", 
+      texture : basePath+"/product/trackter_3.png",
+      video : [],
+      url : linkPath+"/product/3"
+    }, 
+    {
+      type : 'product',
+      title : "kubota 4", 
+      texture : basePath+"/product/trackter_4.png",
+      video : [],
+      url : linkPath+"/product/4"
+    }, 
+    {
+      type : 'product',
+      title : "kubota 5", 
+      texture : basePath+"/product/trackter_5.png",
+      video : [],
+      url : linkPath+"/product/5"
+    },
+    {
+      type : 'product',
+      title : "kubota 6", 
+      texture : basePath+"/product/trackter_6.png",
+      video : [],
+      url : linkPath+"/product/6"
+    },
+    {
+      type : 'product',
+      title : "kubota 7", 
+      texture : basePath+"/product/trackter_7.png",
+      video : [],
+      url : linkPath+"/product/7"
     }
-  });
+  ];
+  const picture = [basePath+"/picture-01.png", basePath+"/picture-02.png"];
+  const presenter = items.find((item, itemKey) => item.type == 'presenter');
 
-  if(items.length%2 > 0){
-    items.push({
-      id:-1,
-      type : 'space',
-      title:"",
-      texture:"",
-      video:[],
-      url:""
-    });
-  }
-  const picture = [productList.image_showroom.left_wall_image, productList.image_showroom.right_wall_image];
-  const presenter = productList.products.find((item, itemKey) => item.type == 'person');
+  // let items = [];
+  // productList.products.map((item, itemKey) => {
+  //   if(item.type == "product"){
+  //     if(item.image_showroom){
+  //       items.push({
+  //         id:item.id,
+  //         type : item.type,
+  //         title:"",
+  //         texture:item.image_showroom,
+  //         video:[],
+  //         url:`${linkPath}/product/${item.id}`
+  //       });
+  //     }
+  //   }else if(item.type == "person") {
+  //     items.push({
+  //       id:item.id,
+  //       type : item.type,
+  //       title:"",
+  //       texture:item.person.person_image,
+  //       video:[
+  //         (item.person.person_video_rgb ? (item.person.person_video_rgb):('')), 
+  //         (item.person.person_video_alpha ? (item.person.person_video_alpha):(''))
+  //       ],
+  //       url:`${linkPath}/product/${item.id}`
+  //     });
+  //   }
+  // });
+
+  // if(items.length%2 > 0){
+  //   items.push({
+  //     id:-1,
+  //     type : 'space',
+  //     title:"",
+  //     texture:"",
+  //     video:[],
+  //     url:""
+  //   });
+  // }
+  // const picture = [productList.image_showroom.left_wall_image, productList.image_showroom.right_wall_image];
+  // const presenter = productList.products.find((item, itemKey) => item.type == 'person');
 
   return (
     <ShowroomInfinity items={items} basePath={basePath} picture={picture} presenter={presenter} />
