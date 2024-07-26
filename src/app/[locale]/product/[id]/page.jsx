c87a58c4385d9e7278f1af7e04486d63ff840b9a
trirong <trirong_berm@hotmail.com>
2024-07-26 15:11:57 +0700

    change name product

--- src/app/[locale]/product/[id]/page.jsx
+++ src/app/[locale]/product/[id]/page.jsx
@@ -22,7 +22,7 @@ export async function generateMetadata({ params, searchParams }, parent) {
   if(metaData.og_url){
     og_url = metaData.og_url.replace("https://skbt-main.digi-team.work/onlineshowroom-backend/products/", "");
     og_url = og_url.replace("/");
-    og_url = `${process.env.SKBT_HTTP_HOST}${process.env.SKBT_SUBFOLDER}/${og_url}`;
+    og_url = `${process.env.SKBT_HTTP_HOST}${process.env.SKBT_SUBFOLDER}/product/${og_url}`;
   }
   
   
