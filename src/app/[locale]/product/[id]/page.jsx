7f9d7455d35164652e0f7b7d47a2c2517f0dfbb2
trirong <trirong_berm@hotmail.com>
2024-07-26 15:04:25 +0700

    url env og

--- src/app/[locale]/product/[id]/page.jsx
+++ src/app/[locale]/product/[id]/page.jsx
@@ -20,7 +20,7 @@ export async function generateMetadata({ params, searchParams }, parent) {
 
   let og_url = '';
   if(metaData.og_url){
-    og_url = metaData.og_url.replace("https://skbt-main.digi-team.work/onlineshowroom-backend/products/", "https://skbt-main.digi-team.work/onlineshowroom/product/");
+    og_url = metaData.og_url.replace("https://skbt-main.digi-team.work/onlineshowroom-backend/products/", `${process.env.SKBT_HTTP_HOST}${process.env.SKBT_SUBFOLDER}/`);
   }
   
   
