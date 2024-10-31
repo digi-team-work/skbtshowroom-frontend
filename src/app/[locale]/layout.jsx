993611b093634bc8c6465340ab03e32e4173ce52
Narenrit Hadsadintorn <kie@adyim.com>
2024-10-31 16:37:21 +0700

    [hotfix] - add gtag nanager

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -4,19 +4,22 @@ import '@/styles/home.css'
 import '@/styles/product.css'
 import '@/styles/r3f.css'
 
-import { MetadataDefault } from "@/lib/metadata";
+import { MetadataDefault } from '@/lib/metadata'
+import { GoogleTagManager } from '@next/third-parties/google'
 
-export const metadata = MetadataDefault;
+export const metadata = MetadataDefault
 
 export default function RootLayout({ children, params: { locale } }) {
-  const basePath = `${process.env.SKBT_BASEPATH}`;
+  const basePath = `${process.env.SKBT_BASEPATH}`
+  const gtm = `${process.env.NEXT_PUBLIC_GTM}`
 
   return (
     <html lang={locale} className='antialiased'>
       <head>
-        <link rel="icon" href={`${basePath}/favicon.ico`} sizes="any" />
+        <link rel='icon' href={`${basePath}/favicon.ico`} sizes='any' />
       </head>
       <body>
+        {gtm !== 'GTM-XXX' && <GoogleTagManager gtmId={`${gtm}`} />}
         <Layout>{children}</Layout>
       </body>
     </html>
